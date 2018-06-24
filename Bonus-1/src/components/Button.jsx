22d16b306f13bab50e5175f1042bd96d9b4f7615
JamesThomasEdwards <digitalmediapro7@gmail.com>
2018-06-24 03:35:21 -0700

    refactored they way the Button components were rendered

--- Bonus-1/src/components/Button.jsx
+++ Bonus-1/src/components/Button.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 
 const Button = (props) => {
     return (
-        <button onClick={props.handeler}>{props.buttonTitles[props.id]}</button>
+        <button
+            onClick={props.handeler}>
+            {props.buttonTitles}
+        </button>
     )
 }
 
--- Bonus-1/src/containers/BoardContainer.jsx
+++ Bonus-1/src/containers/BoardContainer.jsx
@@ -9,21 +9,33 @@ export default class BoardContainer extends Component {
     state = {
         score: 0
     };
+    // increase score by 1
     increaseScore = () => {
-        this.setState(() => ({ score: this.state.score + 1 }));
+        this.setState(() => ({
+            score: this.state.score + 1
+        }));
     };
+    // decrease score by 1
     decreaseScore = () => {
-        this.setState(() => ({ score: this.state.score - 1 }));
+        this.setState(() => ({
+            score: this.state.score - 1
+        }));
     };
+    // resets score to 0
     resetScore = () => {
         this.setState(() => ({ score: 0 }))
     }
     render() {
         const buttonHandelers = [this.increaseScore, this.decreaseScore, this.resetScore]
         const buttonTitles = ['add', 'subtract', 'reset']
+        // creates 3 button components, each with their 
+        // title and click handelers
         const buttons = buttonHandelers.map((ele, ind) => {
-            return <Button id={ind} handeler={ele}
-                buttonTitles={buttonTitles} />
+            return <Button
+                id={ind}
+                key={'button' + ind}
+                handeler={ele}
+                buttonTitles={buttonTitles[ind]} />
         })
         return (
             <div>
